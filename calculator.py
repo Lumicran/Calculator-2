231db5b0f52c85761b778b49b2e1ba23fd5c366f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-06 15:20:56 -0700

    First commit, request user input, split user input.

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+request = input(" ")
+
+while True:
+    split_request = request.split(" ")
+    print(split_request)
+
+
+    break
