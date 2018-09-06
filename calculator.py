13cbfbde490d32e45f00a5f3fccab2db50f4c5ee
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-06 15:54:55 -0700

    Imported Subtraction Function and Changed Integers to Floats

--- calculator.py
+++ calculator.py
@@ -12,15 +12,17 @@ while True:
     split_request = request.split(" ")
     first = split_request[0]
 
-    num1 = int(split_request[1])
-    num2 = int(split_request[2])
+    num1 = float(split_request[1])
+    num2 = float(split_request[2])
 
     if first == 'q':
         break
-        
+
     elif first == "+":
         print(add(num1, num2))
 
+    elif first == "-":
+        print(subtract(num1, num2))
 
     break
 
